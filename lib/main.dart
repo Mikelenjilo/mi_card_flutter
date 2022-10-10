48ecdccb0eb89be9a1f1bfb00a02a86f5be89e33
Massinissa Mouhoub <massipaylancer@gmail.com>
2022-10-10 19:05:57 +0100

    completed version

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,61 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                color: Colors.red,
-                height: double.infinity,
-                width: 30.0,
-                child: const Text("hello"),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Onizuka.jpg'),
               ),
-              Container(
-                color: Colors.blue,
-                height: double.infinity,
-                width: 30.0,
-                child: const Text("hello"),
+              const Text(
+                'Massinissa Mouhoub',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  letterSpacing: 1.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                  thickness: 2,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  color: Colors.white,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'massipaylancer@gmail.com',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.teal[700],
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
